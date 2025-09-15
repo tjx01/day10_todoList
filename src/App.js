45d279aaff1daab5d995053a02969a391d70b431
TULE <TULE@OOCL.COM>
2025-09-15 11:01:42 +0800

    feat: add TodoDetailPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import {useReducer} from "react";
+import {useContext, useReducer} from "react";
 import {todoReducer} from "./reducer/TodoReducer";
 import {TodoContext} from "./contexts/TodoContext";
-import {createBrowserRouter, NavLink, Outlet, RouterProvider} from "react-router";
+import {createBrowserRouter, NavLink, Outlet, RouterProvider, useParams} from "react-router";
 import {ErrorPage} from "./pages/ErrorPage";
 import {HomePage} from "./pages/HomePage";
+import {TodoItem} from "./components/TodoItem";
 
 function DefaultLayout() {
     return <div>
@@ -21,6 +22,18 @@ function DefaultLayout() {
     </div>;
 }
 
+function TodoDetailPage() {
+    const {id} = useParams();
+    const {state} = useContext(TodoContext);
+    const todo = state.filter(item => item.id === parseInt(id));
+    if (todo.length === 0) {
+        return <div>Not Found Todo</div>;
+    }
+    return <div>
+        <TodoItem todo={todo[0]} index={id}/>
+    </div>
+}
+
 const routes = createBrowserRouter([
     {
         path: "/",
@@ -30,12 +43,20 @@ const routes = createBrowserRouter([
             {
                 path: "/",
                 element: <HomePage />
+            },
+            {
+                path: "/todos/:id",
+                element: <TodoDetailPage />
             }
         ]
     }
 ])
 
-export const initState = [];
+export const initState = [
+    {id: 1, text: "学习 React", done: false},
+    {id: 2, text: "学习 Vue", done: true},
+    {id: 3, text: "学习 Angular", done: false}
+];
 
 function App() {
     const [state, dispatch] = useReducer(todoReducer, initState);
