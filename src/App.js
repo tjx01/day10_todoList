34e5411cc59628c83ddcbfbe5dafef691bf2bae1
TULE <TULE@OOCL.COM>
2025-09-16 10:24:36 +0800

    feat: add interceptor  404

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ import {TodoContext} from "./contexts/TodoContext";
 import {RouterProvider} from "react-router";
 import {routes} from "./routes/Routes";
 import {useTodoServers} from "./useTodoServers";
+import '@ant-design/v5-patch-for-react-19';
+import { unstableSetRender } from 'antd';
+import { createRoot } from 'react-dom/client';
 
 function App() {
     const [state, dispatch] = useReducer(todoReducer, []);
     const {loadTodos} = useTodoServers();
+    unstableSetRender((node, container) => {
+        container._reactRoot ||= createRoot(container);
+        const root = container._reactRoot;
+        root.render(node);
+        return async () => {
+            await new Promise((resolve) => setTimeout(resolve, 0));
+            root.unmount();
+        };
+    });
     useEffect(() => {
         loadTodos()
             .then(todos => dispatch({type: "LOAD_TODOS", payload: todos}))
--- src/api/mockApi.js
+++ src/api/mockApi.js
@@ -1,7 +1,27 @@
 import axios from "axios";
+import {message} from "antd";
 
-export const api = axios.create({
+const api = axios.create({
     baseURL: 'https://68c7ac8f5d8d9f51473287bd.mockapi.io/',
     headers: {'Content-Type': 'application/json'},
     timeout: 10_000,
 })
+
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        const {status, data} = error.response;
+        if (status === 404) {
+            console.log(error.response)
+            message
+                .error("请求资源不存在", 10_100)
+                .then(() => {});
+
+        }
+        return Promise.reject(error);
+    }
+);
+
+export {api}
